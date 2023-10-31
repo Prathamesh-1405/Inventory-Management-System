87eaeeb879fd4c99fd1cc82646db31bd343e779f
Prathamesh-1405 <prathameshpardhiye45@gmail.com>
2023-10-31 13:17:59 +0530

    Changed registartion activity

--- app/src/main/java/com/example/inventorysupplychaainmanagement/RegistrationActivity.java
+++ app/src/main/java/com/example/inventorysupplychaainmanagement/RegistrationActivity.java
@@ -10,6 +10,19 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
 public class RegistrationActivity extends AppCompatActivity {
 
     EditText edUsername, edEmail, edPassword, edConfirm;
@@ -45,7 +58,7 @@ public class RegistrationActivity extends AppCompatActivity {
                 String password = edPassword.getText().toString();
                 String email = edEmail.getText().toString();
                 String confirm = edConfirm.getText().toString();
-
+                String baseUrl = "http://13.233.26.112/";
                 Database db = new Database(getApplicationContext(), "inventory", null, 1);
 
                 if(username.length()==0 || password.length()==0 || email.length()==0 || confirm.length()==0){
@@ -54,8 +67,47 @@ public class RegistrationActivity extends AppCompatActivity {
                 else{
                     if(password.compareTo(confirm)==0){
                         if(isValid(password)){
-                            Toast.makeText(getApplicationContext(), "Record Inserted", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(RegistrationActivity.this,LoginActivity.class));
+                            URL url = null;
+                            try {
+                                url = new URL(baseUrl + "/register");
+                                HttpURLConnection client = null;
+                                try {
+                                    client = (HttpURLConnection) url.openConnection();
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                                // on below line setting method as post.
+                                try {
+                                    client.setDoOutput(true);
+                                    client.setDoInput(true);
+                                    client.setRequestMethod("POST");
+                                } catch (ProtocolException e) {
+                                    e.printStackTrace();
+                                }
+                                client.setRequestProperty("Content-Type", "application/json");
+                                client.setRequestProperty("Accept", "application/json");
+                                client.setRequestProperty("source-name", "streamlining-inventory-management");
+                                // on below line setting client.
+                                String jsonInputString = new String("{ username:" + username + "," + "email:" + email + "password:" + password + "}");
+                                OutputStream os = client.getOutputStream();
+                                os.write(jsonInputString.getBytes("UTF-8"));
+                                os.close();
+
+                                InputStream in = new BufferedInputStream(client.getInputStream());
+                                String result = org.apache.commons.io.IOUtils.toString(in, "UTF-8");
+                                JSONObject jsonObject = new JSONObject(result);
+
+                                Toast.makeText(getApplicationContext(), "Record Inserted", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(RegistrationActivity.this,LoginActivity.class));
+                            } catch (MalformedURLException e) {
+                                e.printStackTrace();
+                            } catch (UnsupportedEncodingException e) {
+                                e.printStackTrace();
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
                         }
                         else{
                             Toast.makeText(getApplicationContext(), "Password must contain at least 8 characters, having letter, digit amd special symbol", Toast.LENGTH_LONG).show();
@@ -99,4 +151,3 @@ public class RegistrationActivity extends AppCompatActivity {
         }
     }
 }
-
